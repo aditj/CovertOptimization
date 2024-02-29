00c58229a589e9f245adb15b07b94052e8fb9bd4
Adit <aditjain1980@gmail.com>
2024-02-28 21:01:13 -0500

    added

--- main.py
+++ main.py
@@ -12,35 +12,50 @@ def rosenbrock_grad(x):
     grad[1:-1] = -400*x[1:-1]*(x[2:]-x[1:-1]**2) + 200*(x[1:-1]-x[:-2]**2) - 2*(1-x[1:-1])
     return grad
 
+def rastrigin(x):
+    A = 10
+    return A*x.size + sum(x**2 - A*np.cos(2*np.pi*x))
+
+def rastrigin_grad(x):
+    A = 10
+    grad = 2*x + 2*np.pi*A*np.sin(2*np.pi*x)
+    return grad
+
 ### noisy gradient of the Rosenbrock function
-def noisy_rosenbrock_grad(x, noise_level=1e-2):
+def noisy_grad(x,grad_fn, noise_level=1e-2):
     noise = noise_level * np.random.normal(size=x.size)
-    return rosenbrock_grad(x) + noise
+    return grad_fn(x) + noise
+
+
 
 ### sgd with constant step size for minimizing the Rosenbrock function
-def sgd_rosenbrock(x0, n_iter=100000, step_size=1e-3, noise_level=1e-2):
+def sgd(x0,grad_fn,fn, n_iter=100000, step_size=1e-3, noise_level=5e-2):
     gradient_norms = np.zeros(n_iter)
     x = x0.copy()
     for i in range(n_iter):
-        grad = noisy_rosenbrock_grad(x, noise_level=noise_level)
+        grad = noisy_grad(x,grad_fn, noise_level=noise_level)
         x -= step_size * grad
+        x = np.clip(x,-5,5)
         gradient_norms[i] = np.linalg.norm(grad)
+        if i%1000==0:
+            print(gradient_norms[i],fn(x),np.linalg.norm(x))
     return gradient_norms
 
 def assign_gradient_state(gradient):
 ### 
-    thresholds = [0.1,0.5,1,1.5,2]
+    thresholds = [0,0.5,1]
     return np.digitize(gradient,thresholds)-1
 def compute_transition_probability_matrix(gradient_states):
-    n_states = np.unique(gradient_states).size
+    n_states = np.max(gradient_states)+1
     P = np.zeros((n_states,n_states))
     for i in range(len(gradient_states)-1):
         P[gradient_states[i],gradient_states[i+1]] += 1
     return P
 if __name__ == "__main__":
-    x0 = np.zeros(100)
-    gradient_norms = sgd_rosenbrock(x0)
+    x0 = np.ones(3)
+    gradient_norms = sgd(x0,rastrigin_grad,fn=rastrigin)
     gradient_states = assign_gradient_state(gradient_norms)
+    print(gradient_states)
     P = compute_transition_probability_matrix(gradient_states)
     P = P/P.sum(axis=1,keepdims = True)
     print(P)
