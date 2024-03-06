a7f28b422525db921a1ef9e20c8f15b3c2140e67
Adit <aditjain1980@gmail.com>
2024-03-06 16:17:20 -0500

    reorganized stuff

--- main.py
+++ main.py
@@ -12,13 +12,19 @@ def rosenbrock_grad(x):
     grad[1:-1] = -400*x[1:-1]*(x[2:]-x[1:-1]**2) + 200*(x[1:-1]-x[:-2]**2) - 2*(1-x[1:-1])
     return grad
 
-def rastrigin(x):
-    A = 10
-    return A*x.size + sum(x**2 - A*np.cos(2*np.pi*x))
+def ackley(x):
+    a = 20
+    b = 0.2
+    c = 2 * np.pi
+    return -a * np.exp(-b * np.sqrt(np.sum(x**2) / x.shape[-1])) - np.exp(np.sum(np.cos(c * x)) /x.shape[-1] ) + a + np.exp(1)
 
-def rastrigin_grad(x):
-    A = 10
-    grad = 2*x + 2*np.pi*A*np.sin(2*np.pi*x)
+def ackley_grad(x):
+    a = 20
+    b = 0.2
+    c = 2 * np.pi
+    grad = np.zeros_like(x)
+    d = np.sqrt(np.sum(x**2) / x.shape[-1])
+    grad = -a * (x / (x.shape[-1] * d)) * np.exp(-b * d) - c*(np.sin(c * x) / x.shape[-1]) * np.exp(np.sum(np.cos(c * x)) / x.shape[-1])
     return grad
 
 ### noisy gradient of the Rosenbrock function
@@ -29,33 +35,22 @@ def noisy_grad(x,grad_fn, noise_level=1e-2):
 
 
 ### sgd with constant step size for minimizing the Rosenbrock function
-def sgd(x0,grad_fn,fn, n_iter=100000, step_size=1e-3, noise_level=5e-2):
-    gradient_norms = np.zeros(n_iter)
+def sgd(x0,grad_fn,fn, n_iter=1000, step_size=1e-1, noise_level=1):
     x = x0.copy()
+    argmin_x = x0.copy()
     for i in range(n_iter):
         grad = noisy_grad(x,grad_fn, noise_level=noise_level)
         x -= step_size * grad
-        x = np.clip(x,-5,5)
-        gradient_norms[i] = np.linalg.norm(grad)
-        if i%1000==0:
-            print(gradient_norms[i],fn(x),np.linalg.norm(x))
-    return gradient_norms
+        x = np.clip(x,-32,32)
+        if fn(x) < fn(argmin_x):
+            argmin_x = x.copy()
+        if i%100==0:
+            print(fn(argmin_x),argmin_x,x,grad)
+    
 
-def assign_gradient_state(gradient):
-### 
-    thresholds = [0,0.5,1]
-    return np.digitize(gradient,thresholds)-1
-def compute_transition_probability_matrix(gradient_states):
-    n_states = np.max(gradient_states)+1
-    P = np.zeros((n_states,n_states))
-    for i in range(len(gradient_states)-1):
-        P[gradient_states[i],gradient_states[i+1]] += 1
-    return P
+
 if __name__ == "__main__":
-    x0 = np.ones(3)
-    gradient_norms = sgd(x0,rastrigin_grad,fn=rastrigin)
-    gradient_states = assign_gradient_state(gradient_norms)
-    print(gradient_states)
-    P = compute_transition_probability_matrix(gradient_states)
-    P = P/P.sum(axis=1,keepdims = True)
-    print(P)
+    x0 = np.ones((1,10))*0.8
+    sgd(x0,ackley_grad,fn=ackley)
+    print(ackley(x0+10))
+    
